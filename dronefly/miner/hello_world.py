59d0fbff3bae39a3a7e162d375860bff2af00dc6
Ben Armstrong <synrg@debian.org>
2026-09-13 16:29:33 -0300

    hello_world: use taxon table to map results to partial taxa

--- dronefly/miner/hello_world.py
+++ dronefly/miner/hello_world.py
@@ -1,17 +1,18 @@
 from pyinaturalist import pprint
-from pyinaturalist_convert import load_fts_taxa, TaxonAutocompleter
+from pyinaturalist_convert import enable_logging, get_db_taxa, load_dwca_taxa, load_fts_taxa, TaxonAutocompleter
 
+enable_logging()
+load_dwca_taxa()
 load_fts_taxa(languages=['english', 'german'])
 ta = TaxonAutocompleter()
 searches = [
     [['aves'], {}],
     [['flughund'], {'language': 'de'}]
 ]
-taxa = []
+taxon_ids = []
 for args, kwargs in searches:
     mat = ta.search(*args, **kwargs)
     if mat:
-        taxa.append(mat[0])
-for taxon in taxa:
-    print(taxon.to_json)
+        taxon_ids.append(mat[0].id)
+for taxon in get_db_taxa(ids=taxon_ids):
     pprint(taxon)
